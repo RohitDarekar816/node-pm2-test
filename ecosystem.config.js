39d9662957925f6601afa309d41ef5e473404428
rohitdarekar816 <rohitdarekar816@gmail.com>
2025-09-24 16:10:37 +0530

    chore: path change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
   deploy: {
     production: {
       user: "root",
-      key: "/home/rohitdarekar/learn/node/sshkey",
+      key: "/home/rohitdarekar/learn/sshkey",
       host: "143.244.131.199",
       ref: "origin/main",
       repo: "https://github.com/RohitDarekar816/node-pm2-test.git",
