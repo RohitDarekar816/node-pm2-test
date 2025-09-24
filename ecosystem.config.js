8ab1536238caf58f9a8aa5267de5f2dcf77f8fc4
rohitdarekar816 <rohitdarekar816@gmail.com>
2025-09-24 16:06:14 +0530

    chore: new server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,10 +15,10 @@ module.exports = {
     production: {
       user: "root",
       key: "/home/rohitdarekar/learn/node/sshkey",
-      host: "142.93.219.250",
+      host: "143.244.131.199",
       ref: "origin/main",
       repo: "https://github.com/RohitDarekar816/node-pm2-test.git",
-      path: "/root",
+      path: "/root/rohit",
       "post-deploy": "npm install",
     },
   },
